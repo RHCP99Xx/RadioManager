12eee8257ca850ff38b247fa10ae8291430917cb
Salomon-JC <salomon_jc@hotmail.com>
2021-11-17 16:30:21 -0600

    Plantillas

---
+++ RadioManager/Models/BD/ConexiónDB.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace RadioManager.Models.BD
+{
+    public class ConexionDB
+    {
+        private static string server = "maisonbleue2020.ddns.net";
+        private static string database = "radio_manager";
+        private static string user = "edgarhv";
+        private static string password = "30dpr4319n#M";
+
+        private static string connectionString = connectionString = "SERVER=" + server + "; PORT = 3306 ;" +
+            "DATABASE=" + database + ";" + "UID=" + user + ";" + "PASSWORD=" + password + ";";
+
+        public static MySqlConnection getConnection()
+        {
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection();
+                connection.ConnectionString = connectionString;
+                connection.Open();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("\nExcepcion Conexión BD");
+                Console.WriteLine(exception.Message);
+                throw;
+            }
+            Console.WriteLine("\nConexión exitosa\n");
+            return connection;
+        }
+    }
+}
