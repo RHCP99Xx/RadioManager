fa9a8a722f0fb5251cef3847175060997abdbb43
Edgar Hernández <47510635+RHCP99Xx@users.noreply.github.com>
2021-12-02 18:18:19 -0600

    All my use cases are working
    Complete UC:
    Lista de usuarios
    Consultar usuario
    Desactivar usuario
    Agregar Usuario
    Login

---
+++ RadioManager/GUI/EditarUsuario.xaml.cs
@@ -0,0 +1,187 @@
+﻿using RadioManager.Models.POCO;
+using System.Windows;
+using RadioManager.Helpers.Validadores;
+using RadioManager.Helpers.EncryptDataPackage;
+using RadioManager.Models.DAO;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace RadioManager.GUI
+{
+    /// <summary>
+    /// Lógica de interacción para EditarUsuario.xaml
+    /// </summary>
+    public partial class EditarUsuario : Window
+    {
+        private Creativo creativoSeleccionado = new Creativo();
+        private Operador operadorSeleccionado = new Operador();
+        public EditarUsuario(Creativo creativo)
+        {
+            this.creativoSeleccionado = creativo;
+            InitializeComponent();
+            llenarCamposCreativo(creativo);
+            this.windowTitle.Text = "Editar Creativo";
+            this.confirmButton.Click += confirmarCambiosCreativo_Button;
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        public EditarUsuario(Operador operador)
+        {
+            this.operadorSeleccionado = operador;
+            InitializeComponent();
+            llenarCamposOperador(operador);
+            this.windowTitle.Text = "Editar Operador";
+            this.confirmButton.Click += confirmarCambiosOperador_Button;
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+
+        public void llenarCamposCreativo(Creativo creativo)
+        {
+            this.nameBox.Text = creativo.Nombre;
+            this.lastNameBox.Text = creativo.Apellidos;
+            this.emailBox.Text = creativo.Correo;
+            this.password.Text = EncryptData.Decrypt(creativo.Contrasenia);
+            this.usernameBox.Visibility = Visibility.Visible;
+            this.usernameBox.Text = creativo.Username;
+        }
+
+        public void llenarCamposOperador(Operador operador)
+        {
+            this.nameBox.Text = operador.Nombre;
+            this.lastNameBox.Text = operador.Apellidos;
+            this.emailBox.Text = operador.Correo;
+            this.password.Text = EncryptData.Decrypt(operador.Contrasenia);
+            this.personalNumberBox.Visibility = Visibility.Visible;
+            this.personalNumberBox.Text = operador.NumPersonal;
+        }
+
+
+        private void confirmarCambiosCreativo_Button(object sender, RoutedEventArgs e)
+        {
+            string nombre = this.nameBox.Text;
+            string apellido = this.lastNameBox.Text;
+            string correo = this.emailBox.Text;
+            string nombreDeUsuario = this.usernameBox.Text;
+            string password = this.password.Text;
+            if (Validator.EmailStructureValidator(correo))
+            {
+                if (!string.IsNullOrWhiteSpace(nombre) && !string.IsNullOrWhiteSpace(apellido) && !string.IsNullOrWhiteSpace(password) && !string.IsNullOrWhiteSpace(nombreDeUsuario))
+                {
+                    if (Validator.checkPasswordLength(password))
+                    {
+                        creativoSeleccionado.Nombre = nombre;
+                        creativoSeleccionado.Apellidos = apellido;
+                        creativoSeleccionado.Correo = correo;
+                        creativoSeleccionado.Username = nombreDeUsuario;
+                        creativoSeleccionado.Contrasenia = EncryptData.Encrypt(password);
+
+                        UsuarioDAO usuarioDAO = new UsuarioDAO();
+                        bool creativoEditado = usuarioDAO.editarCreativo(creativoSeleccionado);
+                        if (creativoEditado)
+                        {
+                            MessageBox.Show("El creativo se ha editado satisfactoriamente", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ocurrio un error al modificar, inténtelo más tarde", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("La contraseña debe tener al menos 8 caracteres", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        this.password.BorderBrush = Brushes.Red;
+                        this.nameBox.BorderBrush = Brushes.Black;
+                        this.lastNameBox.BorderBrush = Brushes.Black;
+                        this.emailBox.BorderBrush = Brushes.Black;
+                        this.personalNumberBox.BorderBrush = Brushes.Black;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Algunos campos están vacíos, inténtalo nuevamente", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    this.password.BorderBrush = Brushes.Red;
+                    this.nameBox.BorderBrush = Brushes.Red;
+                    this.lastNameBox.BorderBrush = Brushes.Red;
+                    this.emailBox.BorderBrush = Brushes.Black;
+                    this.personalNumberBox.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("El correo electrónico no tiene una estructura válida", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                this.password.BorderBrush = Brushes.Black;
+                this.nameBox.BorderBrush = Brushes.Black;
+                this.lastNameBox.BorderBrush = Brushes.Black;
+                this.emailBox.BorderBrush = Brushes.Red;
+                this.personalNumberBox.BorderBrush = Brushes.Black;
+            }
+        }
+
+        private void confirmarCambiosOperador_Button(object sender, RoutedEventArgs e)
+        {
+            string nombre = this.nameBox.Text;
+            string apellido = this.lastNameBox.Text;
+            string correo = this.emailBox.Text;
+            string numeroPersonal = this.personalNumberBox.Text;
+            string password = this.password.Text;
+            if (Validator.EmailStructureValidator(correo))
+            {
+                if (!string.IsNullOrWhiteSpace(nombre) && !string.IsNullOrWhiteSpace(apellido) && !string.IsNullOrWhiteSpace(password) && !string.IsNullOrWhiteSpace(numeroPersonal))
+                {
+                    if (Validator.checkPasswordLength(password))
+                    {
+                        operadorSeleccionado.Nombre = nombre;
+                        operadorSeleccionado.Apellidos = apellido;
+                        operadorSeleccionado.Correo = correo;
+                        operadorSeleccionado.NumPersonal = numeroPersonal;
+                        operadorSeleccionado.Contrasenia = EncryptData.Encrypt(password);
+
+                        UsuarioDAO usuarioDAO = new UsuarioDAO();
+                        bool operadorEditado = usuarioDAO.editarOperador(operadorSeleccionado);
+                        if (operadorEditado)
+                        {
+                            MessageBox.Show("El operador de cabina se ha editado satisfactoriamente", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ocurrio un error al modificar, inténtelo más tarde", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("La contraseña debe tener al menos 8 caracteres", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        this.password.BorderBrush = Brushes.Red;
+                        this.nameBox.BorderBrush = Brushes.Black;
+                        this.lastNameBox.BorderBrush = Brushes.Black;
+                        this.emailBox.BorderBrush = Brushes.Black;
+                        this.personalNumberBox.BorderBrush = Brushes.Black;
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Algunos campos están vacíos, inténtalo nuevamente", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    this.password.BorderBrush = Brushes.Red;
+                    this.nameBox.BorderBrush = Brushes.Red;
+                    this.lastNameBox.BorderBrush = Brushes.Red;
+                    this.emailBox.BorderBrush = Brushes.Black;
+                    this.personalNumberBox.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("El correo electrónico no tiene una estructura válida", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                this.password.BorderBrush = Brushes.Black;
+                this.nameBox.BorderBrush = Brushes.Black;
+                this.lastNameBox.BorderBrush = Brushes.Black;
+                this.emailBox.BorderBrush = Brushes.Red;
+                this.personalNumberBox.BorderBrush = Brushes.Black;
+            }
+        }
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+        }
+    }
+}
--- RadioManager/GUI/Login.xaml.cs
+++ RadioManager/GUI/Login.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
 using RadioManager.Models.BD;
+using RadioManager.Models.DAO;
+using RadioManager.Models.POCO;
 using System;
 using System.Windows;
-
+using RadioManager.Helpers.Validadores;
+using System.Windows.Media;
 
 namespace RadioManager.GUI
 {
@@ -19,19 +22,79 @@ namespace RadioManager.GUI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
+            string email = this.emailBox.Text;
+            string password = this.passwordBox.Password;
 
-            try
-            {
-                MySqlConnection conn = null;
-                conn = ConexionDB.getConnection();
-                
-            }
-            catch (Exception)
-            {
+            UsuarioDAO usuario = new UsuarioDAO();
 
-                throw;
+            if (Validator.EmailStructureValidator(email))
+            {
+                if (!string.IsNullOrEmpty(password))
+                {
+
+                    bool isLogged = usuario.login(email, password);
+                    if (isLogged)
+                    {
+                        Creativo creativoPOCO = usuario.esCreativoLogin(email);
+
+                        if (creativoPOCO.Nombre != null)
+                        {
+                            if (creativoPOCO.Activo)
+                            {
+                                ArtistMainWindow mainArtist = new ArtistMainWindow();
+                                this.Hide();
+                                mainArtist.Show();
+                            }
+                            else
+                            {
+                                MessageBox.Show("El usuario no se encuentra activo, por favor contacte al administrador del sistema", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            Operador operadorPOCO = usuario.esOperadorLogin(email);
+                            if (operadorPOCO.Nombre != null)
+                            {
+                                if (operadorPOCO.Activo)
+                                {
+                                    IMenuCanciones menuCanciones = new IMenuCanciones();
+                                    this.Hide();
+                                    menuCanciones.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("El usuario no se encuentra activo, por favor contacte al administrador del sistema", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                }
+                            }
+                            else
+                            {
+                                Usuario usuarioPOCO = usuario.esAdminLogin(email);
+                                if (usuarioPOCO.Nombre != null)
+                                {
+                                    ListaDeUsuarios listaUsuarios = new ListaDeUsuarios();
+                                    this.Hide();
+                                    listaUsuarios.Show();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("No se ha encontrado ningún usuario con las credenciales proporcionadas.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Algunos campos se encuentran vacíos", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        this.emailBox.BorderBrush = Brushes.Black;
+                        this.passwordBox.BorderBrush = Brushes.Red;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El correo electrónico no tiene una estructura correcta", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    this.emailBox.BorderBrush = Brushes.Red;
+                    this.passwordBox.BorderBrush = Brushes.Black;
+                }
             }
         }
     }
